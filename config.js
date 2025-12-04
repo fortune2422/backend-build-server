b5bec0bb541c7007dda8d66fd505b30f5dba86b1
fortune2422 <fortuntestccount242@gmail.com>
2025-12-04 20:43:46 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    androidProjectPath: "./MyWebviewApp_Fixed3",
+    appPath: "./MyWebviewApp_Fixed3/app",
+    
+    default: {
+        appName: "My App",
+        packageName: "com.example.app",
+        
+        adjustAppToken: "",
+        adjustEvents: {}
+    },
+
+    paths: {
+        manifest: "./MyWebviewApp_Fixed3/app/src/main/AndroidManifest.xml",
+        mainActivity: "./MyWebviewApp_Fixed3/app/src/main/java",
+        googleJson: "./MyWebviewApp_Fixed3/app/google-services.json",
+        appIcon: "./MyWebviewApp_Fixed3/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png"
+    }
+};
