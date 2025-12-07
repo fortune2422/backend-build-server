3819c5739427ce32e5cf1b8c619b27847c8076c8
fortune2422 <fortuntestccount242@gmail.com>
2025-12-07 14:22:17 +0700

    Update gradlew.bat

--- MyWebviewApp_Fixed3/MyWebviewApp_Fixed3/gradlew.bat
+++ MyWebviewApp_Fixed3/MyWebviewApp_Fixed3/gradlew.bat
@@ -1,3 +1,12 @@
 @echo off
-echo Gradle wrapper not found. Please open project in Android Studio and let it generate the wrapper, or install Gradle 8.5+ locally.
-gradle %*
+set DEFAULT_JVM_OPTS=
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set CLASSPATH=%DIRNAME%gradle\wrapper\gradle-wrapper.jar
+
+set JAVA_EXE=java.exe
+if defined JAVA_HOME (
+  set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+)
+
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
